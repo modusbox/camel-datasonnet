455a2b462d9beaf8d409314e918f4405e0b53252
Jose Montoya <jam01@protonmail.com>
2020-07-18 21:20:36 -0500

    re-introduces lang definition

---
+++ src/main/java/com/modus/camel/datasonnet/language/model/DatasonnetExpression.java
@@ -0,0 +1,60 @@
+package com.modus.camel.datasonnet.language.model;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Expression;
+import org.apache.camel.model.language.ExpressionDefinition;
+import org.apache.camel.spi.Metadata;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * To use Datasonnet scripts in Camel expressions or predicates.
+ */
+@Metadata(firstVersion = "3.3.0", label = "language,datasonnet", title = "Datasonnet")
+@XmlRootElement(name = "datasonnet")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class DatasonnetExpression extends ExpressionDefinition {
+
+    @XmlAttribute(name = "inputMimeType")
+    private String inputMimeType;
+
+    @XmlAttribute(name = "outputMimeType")
+    private String outputMimeType;
+
+    public DatasonnetExpression() {
+    }
+
+    public DatasonnetExpression(String expression) {
+        super(expression);
+    }
+
+    public DatasonnetExpression(Expression expression) {
+        setExpressionValue(expression);
+    }
+
+    @Override
+    public String getLanguage() {
+        return "datasonnet";
+    }
+
+
+    public String getInputMimeType() {
+        return inputMimeType;
+    }
+
+    public void setInputMimeType(String inputMimeType) {
+        this.inputMimeType = inputMimeType;
+    }
+
+    public String getOutputMimeType() {
+        return outputMimeType;
+    }
+
+    public void setOutputMimeType(String outputMimeType) {
+        this.outputMimeType = outputMimeType;
+    }
+
+}
